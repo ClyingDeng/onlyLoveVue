a0753cfe3046b7d22c6269f4734ddd42dedffde8
songjiayi1 <1973761486@qq.com>
2019-10-10 11:30:18 +0800

    轮播图修改

--- onlylovevue/src/views/Personal.vue
+++ onlylovevue/src/views/Personal.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <!-- <Carousel></Carousel> -->
+    <Carousel :imgs="imgs"></Carousel>
+    <asideNav></asideNav>
     <h1>personal</h1>
   </div>
 </template>
+<script>
+export default {
+  data: function(){
+    return{
+      imgs: [
+        { url: require("../assets/imgs/01.jpg"), link: '/content1' },
+        { url: require("../assets/imgs/02.jpg"), link: '/content1' },
+        { url: require("../assets/imgs/03.jpg"), link: '/content1' },
+        { url: require("../assets/imgs/04.jpg"), link: '/content1' }
+      ]
+    }
+  }
+}
+</script>
