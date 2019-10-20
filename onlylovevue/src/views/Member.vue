e1e1ce295105ada3f59d208b85ef315c46c3b7dd
ljxlijiaxin <869537628@qq.com>
2019-10-20 12:06:54 +0800

    李佳欣会员界面

--- onlylovevue/src/views/Member.vue
+++ onlylovevue/src/views/Member.vue
@@ -237,7 +237,7 @@ export default {
   },
   methods:{
    open() {
-        this.$alert('这是一段内容', '标题名称', {
+        this.$alert('续费会员', '成功', {
           confirmButtonText: '确定',
           callback: action => {
             this.$message({
